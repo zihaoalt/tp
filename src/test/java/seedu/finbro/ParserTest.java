b7582b422f4411b6bccbfbfe6647794eb9265011
Wang Zaixi <wangzaixi01@gmail.com>
2026-03-15 22:16:57 +0800

    Add unit test for edit limit command updating limit correctly

--- src/test/java/seedu/finbro/ParserTest.java
+++ src/test/java/seedu/finbro/ParserTest.java
@@ -1,6 +1,7 @@
 package seedu.finbro;
 
 import org.junit.jupiter.api.Test;
+import seedu.finbro.commands.Limit;
 import seedu.finbro.exception.FinbroException;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
@@ -27,5 +28,46 @@ public class ParserTest {
                 Parser.parse("add abc food 15/03/2026", expenses, ui));
 
         assertEquals("Amount must be a number.", exception.getMessage());
+    }
+
+    @Test
+    public void parse_editLimit_replace_limitUpdated() throws FinbroException {
+        ExpenseList expenses = new ExpenseList();
+        TestUi ui = new TestUi();
+
+        // yes: confirm setting initial limit to 500
+        // 3: choose replace
+        // 800: new limit
+        // yes: confirm changing limit to 800
+        ui.setInputs("yes", "3", "800", "yes");
+
+        Limit.setLimit(500.0, ui);
+        Parser.parse("edit limit", expenses, ui);
+
+        assertEquals(800.0, Limit.getLimit());
+    }
+
+    private static class TestUi extends Ui {
+        private String[] inputs;
+        private int index = 0;
+
+        public void setInputs(String... inputs) {
+            this.inputs = inputs;
+            this.index = 0;
+        }
+
+        @Override
+        public String readCommand() {
+            return inputs[index++];
+        }
+
+        @Override
+        public void showLimitEditMenu(double currentLimit) { }
+
+        @Override
+        public void showEnterAmountPrompt(String action) { }
+
+        @Override
+        public void showLimit() { }
     }
 }
