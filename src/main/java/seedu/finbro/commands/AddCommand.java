87489d0e941bc14dc818299968cd9f98514191fe
XuZihao <xuzihao024572@gmail.com>
2026-03-17 03:48:43 +0800

    fix bug on add command data format

--- src/main/java/seedu/finbro/commands/AddCommand.java
+++ src/main/java/seedu/finbro/commands/AddCommand.java
@@ -23,17 +23,16 @@ public class AddCommand extends Command {
             double amount = Double.parseDouble(parts[1]);
             String category = parts[2];
             String dateInput = parts[3];
-            DateTimeFormatter inputFormatter = DateTimeFormatter.ofPattern("dd/MM/yyyy");
-            DateTimeFormatter outputFormatter = DateTimeFormatter.ofPattern("d MMMM yyyy");
+            DateTimeFormatter inputFormatter = DateTimeFormatter.ofPattern("yyyy-MM-dd");
             LocalDate parsedDate;
 
             try {
                 parsedDate = LocalDate.parse(dateInput, inputFormatter);
             } catch (DateTimeParseException e) {
-                throw new FinbroException("Invalid date format! Use dd/MM/yyyy");
+                throw new FinbroException("Invalid date format! Use yyyy-MM-dd");
             }
 
-            String formattedDate = parsedDate.format(outputFormatter);
+            String formattedDate = parsedDate.format(inputFormatter);
             Expense expense = new Expense(amount, category, formattedDate);
             expenses.add(expense);
             ui.showExpenseAdded(expense, expenses.size());
