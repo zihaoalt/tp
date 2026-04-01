a0655e9dea1069a2c59b53d0803d410c91d93503
Albert Lee Kuan Yi <2003lky@gmail.com>
2026-04-01 15:07:34 +0800

    Add Javadoc & update logic in BudgetWarningService

--- src/main/java/seedu/finbro/utils/BudgetWarningService.java
+++ src/main/java/seedu/finbro/utils/BudgetWarningService.java
@@ -9,10 +9,17 @@ import seedu.finbro.ui.Ui;
 public class BudgetWarningService {
 
     //@@author AK47ofCode
+    /**
+     * Checks the current expenditure against the set limit and shows appropriate warnings
+     * if the limit is close to being exceeded or has been exceeded.
+     *
+     * @param expenses
+     * @param ui
+     */
     public void checkAndShowWarnings(ExpenseList expenses, Ui ui) {
         double remaining = expenses.getRemainingExpenditure();
         double limit = Limit.getLimit();
-        if (limit > 0 && expenses.size() > 0) {
+        if (limit > 0 && !expenses.isEmpty()) {
             if (remaining < 0) {
                 ui.showBudgetExceeded(limit);
             } else if (remaining <= 20) {
